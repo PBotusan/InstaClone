a5412a52f1c6329e13aa546187039e99a6eb98e5
Paul B <p_botusan@live.nl>
2019-12-05 13:19:21 +0100

    postcontroller for Store

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,11 +2,13 @@
 
 @section('content')
 <div class="container">
-    <form action="">
+    <div> <h1> Add new Post</h1></div>.
+    <form action="/p" enctype="multipart/form-data" method="post">
+    @csrf
         <div class="row">
-            <div class="col-8 offset-2"> 
+            <div class="col-8"> 
                 <div class="form-group row">
-                    <label for="caption" class="col-md-4 col-form-label text-md-left">Post Caption</label>
+                    <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
                     <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" name="caption" value="{{ old('caption') }}" required autocomplete="caption" autofocus>
 
                     @if($errors->has('caption'))
@@ -22,11 +24,15 @@
             <label for="image" class="col-md-4 col-form-label text-md-left">Post Image</label>
             <input type="file" class="form-control-file" id="image" name="image">
             
-             @if($errors->has('image'))
+            @if($errors->has('image'))
                 <span class="invalid-feedback" role="alert">
                       <strong>{{ $errors->first('image') }}</strong>
                 </span>
             @endif
+        </div>
+        
+        <div class="row pt-3">
+            <button class="btn btn-primary"> Add New</button>
         </div>
     </form>
 </div>
