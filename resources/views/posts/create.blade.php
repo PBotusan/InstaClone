a95d2e9af5a4dcaf39052796ee93068adeb0a37d
Paul B <p_botusan@live.nl>
2019-12-05 12:56:52 +0100

    image en caption importen

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,18 +9,24 @@
                     <label for="caption" class="col-md-4 col-form-label text-md-left">Post Caption</label>
                     <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" name="caption" value="{{ old('caption') }}" required autocomplete="caption" autofocus>
 
-                    @error('caption')
+                    @if($errors->has('caption'))
                         <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
+                            <strong>{{ $errors->first('caption') }}</strong>
                         </span>
-                    @enderror
+                    @endif
                 </div>
             </div> 
         </div>
         
         <div class="row">
-            <label for="caption" class="col-md-4 col-form-label text-md-left">Post Caption</label>
+            <label for="image" class="col-md-4 col-form-label text-md-left">Post Image</label>
             <input type="file" class="form-control-file" id="image" name="image">
+            
+             @if($errors->has('image'))
+                <span class="invalid-feedback" role="alert">
+                      <strong>{{ $errors->first('image') }}</strong>
+                </span>
+            @endif
         </div>
     </form>
 </div>
